a0911a111c4af0941b4a1caa6a791782360591a6
guandaxia <guansixu@gmail.com>
2017-12-28 22:16:14 +0800

    修改乘车记录，增加分页功能

--- pages/busRecord/busRecord.js
+++ pages/busRecord/busRecord.js
@@ -48,45 +48,74 @@ Page({
       page
     }
     http(url, data, 'POST').then((res)=>{
+      if(res.responseCode != "000000"){
+        return
+      }
 
-      let recordList = res.resList
+      let recordList = this.data.recordList
+      let resList = this.formatRecord(res.resList)
+      console.log(resList)
+      if(page == 1){
+        recordList = resList
+      }
+      else {
+      
+        recordList.push(...resList);
+      }
+      
+      let hasNextPage = true
+      if(res.page == page){
+        //最后一页
+        hasNextPage = false        
+      }
 
+      page++
       this.setData({
         isLoading: false,
-        page: page++,
-        recordList
+        page,
+        recordList,
+        hasNextPage
       })
 
       this.sortRecord()
-      this.formatRecord()
+      // this.formatRecord()
 
     })
-
-
   },
-  formatRecord(){
-    let recordList = [], recordInfo = {}
+  // mergeRecord(record1, record2){
+  //   let recordList = []
+  //   record1.forEach((element)=>{
+  //     record2.forEach((item)=>{
+  //       if(item.month == element.month){
+          
+  //       }
+  //     })
+  //   })
+
+  // },
+  formatRecord(recordListIn){
+    let recordList = [], recordListOut=[], recordInfo = {}
     let month = ''
     let find = 0;
 
-    this.data.recordList.forEach((element) => {
-      let money = formatMoney(element.transamt)
-      recordInfo.money = money
-      recordInfo.date = element.transtime
+    recordListIn.forEach((element) => {
+      let transamt = formatMoney(element.transamt)
+      recordInfo = {}
+      recordInfo.transamt = transamt  //金额
+      recordInfo.transtime = element.transtime  //日期
 
-      
-      month = recordInfo.date.substring(0, 2)
+      month = recordInfo.transtime.substring(5, 7)
       console.log(month)
       
       find = 0;
-      recordList.forEach((item, key)=>{
+      recordListOut.forEach((item, key)=>{
         if(item.month == month){
-          recordList[key]['recordInfo'].push(recordInfo)
+          recordListOut[key]['recordInfo'].push(recordInfo)
           find = 1;
         }
       })
       if(find == 0){
-        recordList.push({
+        recordListOut.push({
           'month': month,
           'recordInfo':[
             recordInfo
@@ -95,15 +124,18 @@ Page({
       }
       
     })
-    console.log(recordList)
-    this.setData({
-      recordList
-    })
+    console.log(recordListOut)
+    return recordListOut
+
   },
   sortRecord(){
     let recordList = this.data.recordList
     recordList.sort((a, b)=>{
-      return a.date > b.date ? -1 : 1
+      // return a.date > b.date ? -1 : 1
+      let aDate = new Date(a.transtime),
+        bDate = new Date(b.transtime)
+      return aDate.getTime() > bDate.getTime() ? -1 : 1
+
     })
     this.setData({
       recordList
